ff5ed2ba0805c9f23caa39a184eecf08b2ab4519
Peter-McKinney <45462076+Peter-McKinney@users.noreply.github.com>
2023-10-10 23:13:47 -0500

    Add nginx conf, update endpoint prefix

--- index.js
+++ index.js
@@ -1,25 +1,51 @@
 import Fastify from 'fastify';
 import cors from '@fastify/cors';
+import { exec } from 'child_process';
 
 const fastify = Fastify({
   logger: true,
 });
 
 await fastify.register(cors, {
-  origin: ['http://localhost:8080'],
+  origin: ['http://localhost:8080', 'http://petemaison.duckdns.org'],
   methods: ['GET', 'HEAD', 'POST', 'PUT', 'DELETE', 'PATCH'],
 });
 
-fastify.get('/lux', async function handler(request, reply) {
-  const randomInt = Math.floor(Math.random() * (88000 - 0 + 1)) + 0;
-  return { lux: randomInt };
+fastify.get('/api/lux', async function handler(request, reply) {
+  const execPromise = new Promise((resolve, reject) => {
+    exec('python ~/projects/light/lux_reader.py', (error, stdout, stderr) => {
+      if (error) {
+        console.error(`exec error: ${error}`);
+        reply
+          .status(500)
+          .send({ error: 'Failed to execute light reader script' });
+        reject();
+      }
+
+      const luxLine = stdout
+        .split('\n')
+        .find((line) => line.startsWith('Lux:'));
+      console.log(luxLine, 'luxLine');
+
+      if (!luxLine) {
+        reply.status(500).send({ error: 'Failed to retrieve Lux value' });
+        reject();
+      }
+
+      const luxValue = luxLine.split(':')[1].trim();
+      reply.send({ lux: luxValue });
+      resolve();
+    });
+  });
+
+  return execPromise;
 });
 
-fastify.get('/lux/max', async function handler(request, reply) {
+fastify.get('/api/lux/max', async function handler(request, reply) {
   return { lux: 88000 };
 });
 
-fastify.get('/lux/min', async function handler(request, reply) {
+fastify.get('/api/lux/min', async function handler(request, reply) {
   return { lux: 0 };
 });
 
